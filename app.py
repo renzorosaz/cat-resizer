b65a2120b28772822ae1718331bdb5d10e02caba
unknown <Administrator@JOEL.Ibaceta.com>
2021-05-24 19:51:38 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask.globals import request
+from PIL import Image
+from io import BytesIO
+from flask.helpers import send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "hello world"
+
+@app.route('/cat.jpg')
+def cat():
+    width = request.args.get('width')
+    height = request.args.get('height')
+
+    size = (int(width), int(height))
+
+    img = Image.open('gato.jpg')
+    img.thumbnail(size)
+    img_io = BytesIO()
+    img.save(img_io, "JPEG")
+    img_io.seek(0)
+    return send_file(img_io, mimetype="image/jpeg")
